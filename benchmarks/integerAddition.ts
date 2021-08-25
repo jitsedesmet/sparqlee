e40624faad0b49a742639ce0671497e1eec58b4a
Jitse De Smet <jitse@reedonk8.be>
2021-08-25 10:54:52 +0200

    Benchmark before overloadTree

---
+++ benchmarks/integerAddition.ts
@@ -0,0 +1,43 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-console */
+
+import type * as RDF from '@rdfjs/types';
+import type { Event } from 'benchmark';
+import { Suite } from 'benchmark';
+import * as Benchmark from 'benchmark';
+import { DataFactory } from 'rdf-data-factory';
+import { translate } from 'sparqlalgebrajs';
+import { SyncEvaluator } from '../lib/evaluators/SyncEvaluator';
+import { Bindings } from '../lib/Types';
+import { TypeURL } from '../lib/util/Consts';
+import { template } from '../test/util/Aliases';
+
+const benchSuite = new Suite();
+const DF = new DataFactory();
+
+function integerTerm(int: number): RDF.Term {
+  return DF.literal(int.toString(), DF.namedNode(TypeURL.XSD_INTEGER));
+}
+
+const noCache = new Benchmark('bench addition no overloadCache', () => {
+  const query = translate(template('?a + ?b = ?c'));
+  const evaluator = new SyncEvaluator(query.input.expression);
+  const max = 100;
+  for (let fst = 0; fst < max; fst++) {
+    for (let snd = 0; snd < max; snd++) {
+      evaluator.evaluate(Bindings({
+        '?a': integerTerm(fst),
+        '?b': integerTerm(snd),
+        '?c': integerTerm(fst + snd),
+      }));
+    }
+  }
+});
+
+benchSuite.push(noCache);
+benchSuite.on('cycle', (event: Event) => {
+  console.log(String(event.target));
+}).on('complete', () => {
+  console.log(`Mean execution time without cache ${noCache.stats.mean}`);
+  console.log(`Fastest is ${benchSuite.filter('fastest').map('name')}`);
+}).run({ async: true });
